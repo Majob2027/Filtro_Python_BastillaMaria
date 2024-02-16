14a6654d76db44bb4d6e69e42f03d64d21730bfb
Majob2027 <mbastilla2007@gmail.com>
2024-02-16 07:20:42 -0500

    menu

---
+++ main.py
@@ -0,0 +1,43 @@
+print ("***********************************")
+print ("*    Bienvenido administrador     *")
+print ("***********************************")
+
+print("  Que desea hacer ")
+
+print("1. Modulo usuarios")
+print("2. Módulo de Bonificaciones para Clientes Leales")
+print("3. Modulo Gestion de servicios")
+print("4. Modulo reportes")
+decision_principal=int(input("--->"))
+
+
+if decision_principal==1:
+ print ("***********************************")
+ print ("*          Modulo usuarios        *")
+ print ("***********************************")
+ print("1. Registro y Gestión de Usuarios")
+ print("2. Seguimiento del Historial de Usuarios")
+ print("3. Personalización de Servicios")
+ print("4. Gestión de la Fidelización de Clientes")
+
+elif decision_principal==2:
+ print ("*************************************************")
+ print ("* Módulo de Bonificaciones para Clientes Leales *")
+ print ("*************************************************")
+
+elif decision_principal==2:
+ print ("******************************")
+ print ("* Modulo Gestion de servicios*")
+ print ("******************************")
+
+
+elif decision_principal==2:
+ print ("******************")
+ print ("* Modulo reportes*")
+ print ("******************")
+ 
+
+
+    
+
+
