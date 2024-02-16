fbbe3fbc4681fafd60e5bb270c76e9aa91e5c3ac
Majob2027 <mbastilla2007@gmail.com>
2024-02-16 08:23:17 -0500

    crud clientes

---
+++ Modulo_usuarios.py
@@ -0,0 +1,129 @@
+def crearusuario ():
+    import json
+    print ("Vamos a añadir un usuario ")
+    with  open ('Datos.json' , 'r') as file:
+        mijson=json.load (file)
+
+     
+    lista_usuarios={}
+
+    lista_usuarios["Identificacion"]= input("Escriba identificacion de usuario : ")
+    lista_usuarios["Nombre"]= input("Escriba nombre del usuario : ")
+    lista_usuarios["Apellido"]= input("Escriba Apelliido  de usuario : ")
+    lista_usuarios["Direccion"]= input("Escriba Direccion de usuario : ")
+    lista_usuarios["Telefono"]= input("Escriba telefono de usuario : ")
+    lista_usuarios["Años con movistar"]= input("Escriba años con movistar:" )
+    print (" Escriba : si 10 años o mas = Clientes leales o entre 3 y 10 años o mas = Clientes regulares o si menos de tres años = Nuevos ") 
+    lista_usuarios["Categoria"]=input ("  Escriba Clientes leales o Clientes regulares o clientes nuevos dependienod a lo mecionado anteriormente :") 
+
+    print ("Usuario creado correctamente ")
+    mijson['Datos'].append (lista_usuarios)
+
+
+    with  open ('Datos.json' , 'w') as file:
+        json.dump (mijson , file , indent=2)
+
+
+
+
+
+def actualizar_usuario ():
+    import json
+    print ("Vamos a actualizar un usuario ")
+    with  open ('Datos.json' , 'r') as file:
+        mijson=json.load (file)
+        lista =mijson['Datos']
+
+    
+    lista = mijson['Datos']
+
+    print("Por favor, escriba el número de identificación de la persona a actualizar:")
+    documento = (input("---> "))
+
+
+    Usuario_encontrado = None
+    for Usuario in lista :
+        if Usuario["Identificacion"] == documento:
+            Usuario_encontrado = Usuario
+            break
+
+    if Usuario_encontrado is not None:
+        print("Seleccione la información a actualizar:")
+        print("1. Nombre")
+        print("2. Apellido")
+        print("3. Direccion")
+        print("4. Telefono")
+        print("5. Años con movistar")
+        print("6. Categoria")
+        print (" Recuerde que si cambia años movistar debera cambiar categoria")
+
+        decision = int(input("---> "))
+
+        if decision == 1:
+            Usuario_encontrado['Nombre'] = input("Escriba el nuevo nombre: ")
+        elif decision == 2:
+            Usuario_encontrado['Apellido'] = input("Escriba el nuevo Apellido: ")
+        if decision == 3:
+            Usuario_encontrado['Direccion'] = input("Escriba la nueva direccion: ")
+        if decision == 4:
+            Usuario_encontrado['Telefono'] = input("Escriba el nuevo Telefono: ")
+        if decision == 5:
+            Usuario_encontrado['Años con movistar'] = input("Escriba Años movistar: ")
+        if decision == 6:
+            Usuario_encontrado['Categoria'] = input("Escriba la nueva categoria: ")
+        print ("Usuario actualizado correctamente ")
+
+    with  open ('Datos.json' , 'w') as file:
+        json.dump (mijson , file , indent=2)
+
+
+
+
+
+def elminarusuario ():
+
+    import json
+    print ("Vamos a Eliminar  un usuario ")
+    with  open ('Datos.json' , 'r') as file:
+        mijson=json.load (file)
+        lista =mijson['Datos']
+
+    print("Por favor, escriba el número de identificación de la persona a Eliminar:")
+    documento = (input("---> "))
+
+
+    Usuario_encontrado = None
+    for Usuario in lista :
+        if Usuario["Identificacion"] == documento:
+            Usuario_encontrado = Usuario
+            break
+
+    if Usuario_encontrado is not None:
+        mijson['Datos'].remove(Usuario_encontrado)
+    
+    print ("Usuario eliminado correctamente ")
+
+    with  open ('Datos.json' , 'w') as file:
+        json.dump (mijson , file , indent=2)
+    
+
+    
+
+
+
+
+
+            
+
+
+    
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,43 +1,79 @@
-print ("***********************************")
-print ("*    Bienvenido administrador     *")
-print ("***********************************")
-
-print("  Que desea hacer ")
-
-print("1. Modulo usuarios")
-print("2. Módulo de Bonificaciones para Clientes Leales")
-print("3. Modulo Gestion de servicios")
-print("4. Modulo reportes")
-decision_principal=int(input("--->"))
+import Modulo_usuarios
 
 
-if decision_principal==1:
- print ("***********************************")
- print ("*          Modulo usuarios        *")
- print ("***********************************")
- print("1. Registro y Gestión de Usuarios")
- print("2. Seguimiento del Historial de Usuarios")
- print("3. Personalización de Servicios")
- print("4. Gestión de la Fidelización de Clientes")
 
-elif decision_principal==2:
- print ("*************************************************")
- print ("* Módulo de Bonificaciones para Clientes Leales *")
- print ("*************************************************")
+while True :
+    print ("***********************************")
+    print ("*    Bienvenido administrador     *")
+    print ("***********************************")
 
-elif decision_principal==2:
- print ("******************************")
- print ("* Modulo Gestion de servicios*")
- print ("******************************")
-
-
-elif decision_principal==2:
- print ("******************")
- print ("* Modulo reportes*")
- print ("******************")
- 
+    print("  Que desea hacer ")
+    print("")
+    print("1. Modulo usuarios")
+    print("")
+    print("2. Módulo de Bonificaciones para Clientes Leales")
+    print("")
+    print("3. Modulo Gestion de servicios")
+    print("")
+    print("4. Modulo reportes")
+    decision_principal=int(input("--->"))
+    print("")
+    print("")
 
 
+    if decision_principal==1:
+        print ("***********************************")
+        print ("*          Modulo usuarios        *")
+        print ("***********************************")
+        print("1. Registro y Gestión de Usuarios")
+        print("2. Seguimiento del Historial de Usuarios")
+        print("3. Personalización de Servicios")
+        print("4. Gestión de la Fidelización de Clientes")
+        decision1=int(input("Elija un numero"))
+        print("")
+
+    if decision1==1 :
+        print (" 1. Agregar usuario")
+        print("")
+        print (" 2. Actualizar usuario ")
+        print("")
+        print (" 3. Eliminar usuario ")
+        print("")
+        decision2=int(input("Elija un opcion "))
+        print("")
+
+    if decision2 == 1 :
+        print("Vamos a agregar un nuevo usuario ")
+        print(Modulo_usuarios.crearusuario())
+        x=input("")
+    if decision2 == 2 :
+        print("Vamos a actualizar")
+        print(Modulo_usuarios.actualizar_usuario())
+        x=input("")
+    if decision2 == 3 :
+        print("Eliminar un usuario")
+        print(Modulo_usuarios.elminarusuario())
+        x=input("")
+        
+
+    elif decision_principal==2:
+        print ("*************************************************")
+        print ("* Módulo de Bonificaciones para Clientes Leales *")
+        print ("*************************************************")
+
+    elif decision_principal==2:
+        print ("******************************")
+        print ("* Modulo Gestion de servicios*")
+        print ("******************************")
+
+
+    elif decision_principal==2:
+        print ("******************")
+        print ("* Modulo reportes*")
+        print ("******************")
     
+
+
+        
 
 
